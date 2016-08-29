d4b75d888d07421b2bbff521a5730623c47986c3
Florian Ingerl <imelflorianingerl@gmail.com>
2016-08-29 21:06:00 +0200

    This is a nice eclipse project with the model stuff of my chess program. The library can be built with gradle. All tests pass.

---
+++ com.florian.chess.core/src/main/java/com/florian/chess/core/MoveEncoding.java
@@ -0,0 +1,6 @@
+package com.florian.chess.core;
+
+public enum MoveEncoding {
+	LONG_ALGEBRAIC_NOTATION,
+	SHORT_ALGEBRAIC_NOTATION
+}
---
+++ com.florian.chess.core/src/main/java/com/florian/chess/core/Pair.java
@@ -0,0 +1,13 @@
+package com.florian.chess.core;
+
+public class Pair<F,S> {
+
+	public F first;
+	public S second;
+	
+	public Pair(F f, S s)
+	{
+		this.first = f;
+		this.second = s;
+	}
+}
